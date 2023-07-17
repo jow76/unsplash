efff3f1a767239a05e1d24bed6803c00dcb05538
jow76 <joejardine76@gmail.com>
2023-07-17 14:17:23 +0100

    Main pls

--- 301/horned-beast/src/App.js
+++ 301/horned-beast/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/Header/Header'
+import './components/Main/Main'
+import './components/Footer/Footer'
+import './components/Horned-Beast/Horned-Beast'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
