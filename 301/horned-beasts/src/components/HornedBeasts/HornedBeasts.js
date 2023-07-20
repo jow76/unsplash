0ca222ac125f5d5de4cd8c886c1f67b88ad7e378
jow76 <joejardine76@gmail.com>
2023-07-20 14:43:54 +0100

    Removing broken features for now to work on today's lab

--- 301/horned-beasts/src/components/HornedBeasts/HornedBeasts.js
+++ 301/horned-beasts/src/components/HornedBeasts/HornedBeasts.js
@@ -4,7 +4,7 @@ export default function HornedBeasts(props) {
 return (
     <div className="hbDiv">
         <h2>{props.title}</h2>
-        <img src={props.imgUrl} alt={props.title} onClick={()=>handleModal(props.beastObject)}></img>
+        <img src={props.imgUrl} alt={props.title}></img>
         <p>{props.description}</p>
     </div>
     )
