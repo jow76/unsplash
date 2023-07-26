b41fa70a1414a19565502107083bbc8c6e1b2131
jow76 <joejardine76@gmail.com>
2023-07-26 15:46:31 +0100

    Basic function implemented

---
+++ 301/unsplash/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css';
+
+import axios from "axios"
+
+function App() {
+
+  const [searchQuery, setsearchQuery] = useState("")
+  const [img, setImg] = useState("")
+
+  function handleSearch(event){
+    setsearchQuery(event.target.value)
+  }
+
+  async function getImage(){
+    try{
+      const API = `http://localhost:8090/photos?query=${searchQuery}`
+      const res = await axios.get(API)
+      // console.log(res.data)
+      setImg(res.data[0].img_url)
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className="App">
+    <h1>Find any image</h1>
+    <input type="text" placeholder="enter image query" onChange={handleSearch}/>
+    <button onClick={getImage}>Explore!</button>
+    {img && <img src={img} alt={searchQuery} />}
+    </div>
+  );
+}
+
+export default App;
